fd47b22035cfba87ad2a783e2871962ba1886a92
Joshua <joshuamdk6@gmail.com>
2020-07-07 20:26:08 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,4 +5,6 @@ client = commands.Bot(command_prefix= "sh!")
 
 @client.event
 async def on_ready():
-    
+    print("Bot has been activated...")
+
+client.run("NzMwMDA0MTAzNzE5Mjg4OTA0.XwRLrw.foAr71OAADWoqveKYo5yi89KaTQ")
