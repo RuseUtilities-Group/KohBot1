392762b0dce19c7db60ff8fe0c919299dbeefd55
Joshua <joshuamdk6@gmail.com>
2020-07-07 21:05:33 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+process.on('unhandledRejection', error => {
+	console.error('Unhandled promise rejection:', error);
+});
+
+client.on('shardError', error => {
+    console.error('A websocket connection encountered an error:', error);
+});
 
 client.login(config.token);
 
@@ -30,19 +37,12 @@ client.on("guildDelete", guild => {
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
-  if (!channel) return;
-  channel.send(`Welcome to this server, ${member}`);
-});
-
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) {
         return
     }
     if (receivedMessage.content.includes(client.user.toString())) {
-        receivedMessage.channel.send("AI Loading...")
+        receivedMessage.channel.send("")
     }
 });
 
@@ -53,25 +53,57 @@ client.on("message", async message => {
   const command = args.shift().toLowerCase();
 
 if(command === "help") {
-    const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-channel.send(exampleEmbed);
+    const exampleEmbed = {
+        color: 0x0099ff,
+        title: 'Some title',
+        url: 'https://discord.js.org',
+        author: {
+            name: 'Some name',
+            icon_url: 'https://i.imgur.com/wSTFkRM.png',
+            url: 'https://discord.js.org',
+        },
+        description: 'Some description here',
+        thumbnail: {
+            url: 'https://i.imgur.com/wSTFkRM.png',
+        },
+        fields: [
+            {
+                name: 'Regular field title',
+                value: 'Some value here',
+            },
+            {
+                name: '\u200b',
+                value: '\u200b',
+                inline: false,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+        ],
+        image: {
+            url: 'https://i.imgur.com/wSTFkRM.png',
+        },
+        timestamp: new Date(),
+        footer: {
+            text: 'Some footer text here',
+            icon_url: 'https://i.imgur.com/wSTFkRM.png',
+        },
+    };
+    
+    client.channel.send({ embed: exampleEmbed });
+    
   }
 
 });
