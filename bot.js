040e45795d7843b7d3920c359f975eec5bec4024
Joshua <joshuamdk6@gmail.com>
2020-07-07 21:09:49 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,17 +54,11 @@ client.on("message", async message => {
 
 if(command === "help") {
     const exampleEmbed = {
-        color: 0x0099ff,
+        color: ,
         title: 'Some title',
-        url: 'https://discord.js.org',
-        author: {
-            name: 'Some name',
-            icon_url: 'https://i.imgur.com/wSTFkRM.png',
-            url: 'https://discord.js.org',
-        },
         description: 'Some description here',
         thumbnail: {
-            url: 'https://i.imgur.com/wSTFkRM.png',
+            url: 'https://cdn.discordapp.com/attachments/694469683281395742/730017345640333369/hitler-at-dortmund-rally.jpg',
         },
         fields: [
             {
@@ -75,34 +69,17 @@ if(command === "help") {
                 name: '\u200b',
                 value: '\u200b',
                 inline: false,
-            },
-            {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-            },
-            {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-            },
-            {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-            },
+            }
         ],
-        image: {
-            url: 'https://i.imgur.com/wSTFkRM.png',
-        },
+      
         timestamp: new Date(),
         footer: {
-            text: 'Some footer text here',
-            icon_url: 'https://i.imgur.com/wSTFkRM.png',
+            text: 'Secret Hitler Bot By RuseUtilities Group',
+            icon_url: 'https://cdn.discordapp.com/attachments/694469683281395742/730017345640333369/hitler-at-dortmund-rally.jpg',
         },
     };
     
-    client.channel.send({ embed: exampleEmbed });
+    message.channel.send({ embed: exampleEmbed });
     
   }
 
