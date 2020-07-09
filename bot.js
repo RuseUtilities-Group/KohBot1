b7c076e9d8c9d7e6c0f24e08bf479d771f165948
Joshua <joshuamdk6@gmail.com>
2020-07-09 22:56:59 +1000

    yes

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const snipes = require("./snipe.json");
 const ytdl = require('ytdl-core');
 
 
