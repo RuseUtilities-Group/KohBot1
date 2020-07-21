9d1987b1c920c234f82c44df0d959c84e5be6010
Joshua <joshuamdk6@gmail.com>
2020-07-22 09:20:40 +1000

    Update index.js

--- index.js
+++ index.js
@@ -261,7 +261,6 @@ if(command === "say") {
 }
 
 
-
 if(command === "ping") {
     const m = await message.channel.send("Ping!");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
