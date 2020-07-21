37a00a67bacd064675a8c731887a4d7931dc8c5d
Joshua <joshuamdk6@gmail.com>
2020-07-22 09:16:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ client.on('message', message => {
 client.on('message', message => {
   if(message.author.bot) return;
   if(message.content.toLowerCase().includes('trump')) { message.channel.send("Trump Gay, Vote Biden")}
-});
+}); 
 
 client.on("message", async message => {
   if(message.author.bot) return;
