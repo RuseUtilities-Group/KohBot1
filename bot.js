26324a0ff339da36dda79d2751d921908e8e4b60
Joshua <joshuamdk6@gmail.com>
2020-07-22 20:40:40 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -277,8 +277,8 @@ if(command === "purge") {
   if(!message.member.roles.some(r=>["Trump Administration", "Admin", "Owner", "Senior Admin/ Vice Owner", "Admin 1", "Admin 2", "Admin 3", "Trial Admin", "Little Warn Man", "Mod", "Moderator"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
     const deleteCount = parseInt(args[0], 10);
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    if(!deleteCount || deleteCount < 2 || deleteCount > 10000)
+      return message.reply("Please provide a number between 2 and 10000 for the number of messages to delete");
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
