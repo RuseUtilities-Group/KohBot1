7c917d15bae00a5fdb8cf0d60bfa5ff4d99ea46a
Joshua <joshuamdk6@gmail.com>
2020-07-22 11:43:21 +1000

    Update index.js

--- index.js
+++ index.js
@@ -267,7 +267,6 @@ if(command === "ping") {
   }
 
 
-
 if(command === "purge") {
   if(!message.member.roles.some(r=>["Trump Administration", "Admin", "Owner", "Senior Admin/ Vice Owner", "Admin 1", "Admin 2", "Admin 3", "Trial Admin", "Little Warn Man", "Mod", "Moderator"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
