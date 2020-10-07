2fe8c898193db6edabc2a0f1318521f9192cbf0d
Joshua <joshuamdk6@gmail.com>
2020-10-07 22:49:06 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -741,7 +741,11 @@ if (command === "massremoverole") {
 
 
 
-
+if (command === "kill"){
+  const user = message.mentions.members.first();
+  message.channel.send(`${user} is now dead.`)
+  
+}
 
 
 
